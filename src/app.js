662eed2417a00d5c659e89723c9cbd4ea12c2266
Channy Somers <cb_somers@hotmail.com>
2018-01-25 18:44:40 -0600

    tests for setExpenses and startSetExpenses

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
@@ -20,6 +20,11 @@ const jsx = (
     </Provider>    
 
 );
+ReactDOM.render(<p>Loading..</p>, document.getElementById('app'));
 
-ReactDOM.render(jsx, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+
   
